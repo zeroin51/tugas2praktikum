1106df85adfec753a24e39b091a53fc5b3419bf9
zeroin51 <davstwn88@gmail.com>
2023-09-20 12:49:57 +0700

    merubah tampil data

--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 class TampilData extends StatelessWidget {
-  final String data; //contoh data yang di-passing
-  
-  const TampilData({Key? key, required this.data}) : super(key: key);
+  final String nama; //contoh data yang di-passing
+  final String nim;
+  final int tahun;
+  const TampilData({Key? key, required this.nama,required this.nim, required this.tahun}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Perkenalan"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Text("Nama saya $nama, NIM $nim, dan umur saya adalah ${DateTime.now().year - tahun} tahun"),
       ),
     );
   }
