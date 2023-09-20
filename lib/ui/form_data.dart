5ea9b98b6e3447208a646f300e612744b530b379
zeroin51 <davstwn88@gmail.com>
2023-09-20 12:51:47 +0700

    merubah form data

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,3 +1,4 @@
+import 'package:tugas_2/ui/tampil_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,12 +10,53 @@ class FormData extends StatefulWidget {
 }
 
 class _FormDataState extends State<FormData> {
+  TextEditingController _namaController = TextEditingController();
+  TextEditingController _nimController = TextEditingController();
+  TextEditingController _tahunController = TextEditingController();
+
+  Widget inputText(TextEditingController textcontroller, String name ){
+    return
+      Padding(
+        padding: EdgeInsets.all(5),
+        child: TextField(
+            controller: textcontroller,
+            decoration: InputDecoration(
+              labelText: name,
+            )
+        )
+      );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Input Data"),
       ),
+      body: Column(
+        children: [
+          inputText(_namaController, "Nama"),
+          inputText(_nimController, "Nim"),
+          inputText(_tahunController, "Tahun Lahir"),
+          ElevatedButton(
+            child: Text("Simpan"),
+            onPressed: (){
+              String nama = _namaController.text;
+              String nim = _nimController.text;
+              int tahun= int.parse(_tahunController.text);
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => TampilData(
+                    nama: nama,
+                    nim: nim,
+                    tahun: tahun,
+                  )
+              ));
+            },
+          )
+
+        ],
+      )
     );
   }
 }
